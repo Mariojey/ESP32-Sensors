3ba2c494daabc998ada42db5f8df4f10c3bfc009
Mariusz Jacek <marios.yaceg@gmail.com>
2025-04-20 00:04:40 +0200

    Gas resustance, temperature, pressure and humidity

---
+++ BME688/BME688.ino
@@ -0,0 +1,225 @@
+#include "Arduino.h"
+#include "bme68xLibrary.h"
+
+#include "FS.h"
+#include "SD.h"
+#include "SPI.h"
+
+#ifndef PIN_CS
+#define PIN_CS 15
+#endif
+
+#define ADD_I2C 0x77
+
+Bme68x bme;
+
+//SD Utils-Functions
+void listDir(fs::FS &fs, const char *dirname, uint8_t levels){
+
+  File root = fs.open(dirname);
+  if(!root){
+    Serial.println("Failed to open direcotry!");
+    return;
+  }
+
+  if(!root.isDirectory()){
+    Serial.println("Not a directory");
+    return;
+  }
+
+  File file = root.openNextFile();
+
+  while(file){
+
+    if(file.isDirectory()){
+      Serial.print("  DIR:  ");
+      Serial.println(file.name());
+      if(levels){
+        listDir(fs, file.path(), levels -1);
+      }
+    }else{
+      Serial.print(" FILE: ");
+      Serial.print(file.name());
+      Serial.print("  SIZE: ");
+      Serial.println(file.size());
+    }
+    file = root.openNextFile();
+  }
+}
+
+void createDir(fs::FS &fs, const char *path) {
+  Serial.printf("Creating Dir: %s\n", path);
+  if (fs.mkdir(path)) {
+    Serial.println("Dir created");
+  } else {
+    Serial.println("mkdir failed");
+  }
+}
+
+void removeDir(fs::FS &fs, const char *path) {
+  Serial.printf("Removing Dir: %s\n", path);
+  if (fs.rmdir(path)) {
+    Serial.println("Dir removed");
+  } else {
+    Serial.println("rmdir failed");
+  }
+}
+
+void readFile(fs::FS &fs, const char *path) {
+  Serial.printf("Reading file: %s\n", path);
+
+  File file = fs.open(path);
+  if (!file) {
+    Serial.println("Failed to open file for reading");
+    return;
+  }
+
+  Serial.print("Read from file: ");
+  while (file.available()) {
+    Serial.write(file.read());
+  }
+  file.close();
+}
+
+void writeFile(fs::FS &fs, const char *path, const char *message) {
+  Serial.printf("Writing file: %s\n", path);
+
+  File file = fs.open(path, FILE_WRITE);
+  if (!file) {
+    Serial.println("Failed to open file for writing");
+    return;
+  }
+  if (file.print(message)) {
+    Serial.println("File written");
+  } else {
+    Serial.println("Write failed");
+  }
+  file.close();
+}
+
+void appendFile(fs::FS &fs, const char *path, const char *message) {
+  Serial.printf("Appending to file: %s\n", path);
+
+  File file = fs.open(path, FILE_APPEND);
+  if (!file) {
+    Serial.println("Failed to open file for appending");
+    return;
+  }
+  if (file.print(message)) {
+    Serial.println("Message appended");
+  } else {
+    Serial.println("Append failed");
+  }
+  file.close();
+}
+
+void renameFile(fs::FS &fs, const char *path1, const char *path2) {
+  Serial.printf("Renaming file %s to %s\n", path1, path2);
+  if (fs.rename(path1, path2)) {
+    Serial.println("File renamed");
+  } else {
+    Serial.println("Rename failed");
+  }
+}
+
+void deleteFile(fs::FS &fs, const char *path) {
+  Serial.printf("Deleting file: %s\n", path);
+  if (fs.remove(path)) {
+    Serial.println("File deleted");
+  } else {
+    Serial.println("Delete failed");
+  }
+}
+
+void testFileIO(fs::FS &fs, const char *path) {
+  File file = fs.open(path);
+  static uint8_t buf[512];
+  size_t len = 0;
+  uint32_t start = millis();
+  uint32_t end = start;
+  if (file) {
+    len = file.size();
+    size_t flen = len;
+    start = millis();
+    while (len) {
+      size_t toRead = len;
+      if (toRead > 512) {
+        toRead = 512;
+      }
+      file.read(buf, toRead);
+      len -= toRead;
+    }
+    end = millis() - start;
+    Serial.printf("%u bytes read for %lu ms\n", flen, end);
+    file.close();
+  } else {
+    Serial.println("Failed to open file for reading");
+  }
+
+  file = fs.open(path, FILE_WRITE);
+  if (!file) {
+    Serial.println("Failed to open file for writing");
+    return;
+  }
+
+  size_t i;
+  start = millis();
+  for (i = 0; i < 2048; i++) {
+    file.write(buf, 512);
+  }
+  end = millis() - start;
+  Serial.printf("%u bytes written for %lu ms\n", 2048 * 512, end);
+  file.close();
+}
+
+void setup() {
+  
+  Wire.begin();
+  Serial.begin(115200);
+
+  bme.begin(ADD_I2C, Wire);
+
+  	if(bme.checkStatus())
+	{
+		if (bme.checkStatus() == BME68X_ERROR)
+		{
+			Serial.println("Sensor error:" + bme.statusString());
+			return;
+		}
+		else if (bme.checkStatus() == BME68X_WARNING)
+		{
+			Serial.println("Sensor Warning:" + bme.statusString());
+		}
+	}
+
+  bme.setTPH();
+
+  bme.setHeaterProf(300, 100);
+
+  appendFile(SD, "/bme688_19_04.txt","Temp, Hum, Press, Res");
+
+
+}
+
+void loop() {
+  
+  bme68xData data;
+
+  bme.setOpMode(BME68X_FORCED_MODE);
+  delay(500+bme.getMeasDur()/200);
+
+  if(bme.fetchData()){
+    Serial.print(String(data.temperature) + ", ");
+		Serial.print(String(data.pressure) + ", ");
+		Serial.print(String(data.humidity) + ", ");
+		Serial.print(String(data.gas_resistance) + ", ");
+		Serial.println(data.status, HEX);
+
+    char buffer[32];
+
+    aprintf(buffer, "%d    %d    %d   %d\n", data.temperature, data.pressure, data.humidity, data.gas_resistance);
+    appendFile(SD, "/bme688_19_04.txt", buffer);
+
+  }
+
+}
