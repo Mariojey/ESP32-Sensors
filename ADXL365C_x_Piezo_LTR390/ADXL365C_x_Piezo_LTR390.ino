b087b223bce516159210aa61a02097c5ffac5397
Mariusz Jacek <marios.yaceg@gmail.com>
2025-04-20 00:03:47 +0200

    UV, acceleration and vib

---
+++ ADXL365C_x_Piezo_LTR390/ADXL365C_x_Piezo_LTR390.ino
@@ -0,0 +1,327 @@
+#include <Arduino.h>
+
+//SD
+#include "FS.h"
+#include "SD.h"
+#include "SPI.h"
+
+//ADXL356C
+#define SERIAL Serial
+#define SYS_VOL 3.3
+
+#define MODULE_RANGE 20
+#define MODULE_VOL 1.8
+
+#define CALI_BUF_LEN        15
+#define CALI_INTERVAL_TIME 100
+
+float cali_buf_xy[CALI_BUF_LEN];
+float cali_buf_z[CALI_BUF_LEN];
+
+float cali_data_xy;
+float cali_data_z;
+int16_t scale;
+
+#define XY_PIN 34
+#define Z_PIN  35
+
+//PIEZO
+#define Piezo_PIN 33
+
+//LTR390
+#include "DFRobot_LTR390UV.h"
+DFRobot_LTR390UV ltr390(LTR390UV_DEVICE_ADDR, &Wire);
+
+//SD Utils-Functions
+void listDir(fs::FS &fs, const char *dirname, uint8_t levels){
+
+  File root = fs.open(dirname);
+  if(!root){
+    Serial.println("Failed to open direcotry!");
+    return;
+  }
+
+  if(!root.isDirectory()){
+    Serial.println("Not a directory");
+    return;
+  }
+
+  File file = root.openNextFile();
+
+  while(file){
+
+    if(file.isDirectory()){
+      Serial.print("  DIR:  ");
+      Serial.println(file.name());
+      if(levels){
+        listDir(fs, file.path(), levels -1);
+      }
+    }else{
+      Serial.print(" FILE: ");
+      Serial.print(file.name());
+      Serial.print("  SIZE: ");
+      Serial.println(file.size());
+    }
+    file = root.openNextFile();
+  }
+}
+
+void createDir(fs::FS &fs, const char *path) {
+  Serial.printf("Creating Dir: %s\n", path);
+  if (fs.mkdir(path)) {
+    Serial.println("Dir created");
+  } else {
+    Serial.println("mkdir failed");
+  }
+}
+
+void removeDir(fs::FS &fs, const char *path) {
+  Serial.printf("Removing Dir: %s\n", path);
+  if (fs.rmdir(path)) {
+    Serial.println("Dir removed");
+  } else {
+    Serial.println("rmdir failed");
+  }
+}
+
+void readFile(fs::FS &fs, const char *path) {
+  Serial.printf("Reading file: %s\n", path);
+
+  File file = fs.open(path);
+  if (!file) {
+    Serial.println("Failed to open file for reading");
+    return;
+  }
+
+  Serial.print("Read from file: ");
+  while (file.available()) {
+    Serial.write(file.read());
+  }
+  file.close();
+}
+
+void writeFile(fs::FS &fs, const char *path, const char *message) {
+  Serial.printf("Writing file: %s\n", path);
+
+  File file = fs.open(path, FILE_WRITE);
+  if (!file) {
+    Serial.println("Failed to open file for writing");
+    return;
+  }
+  if (file.print(message)) {
+    Serial.println("File written");
+  } else {
+    Serial.println("Write failed");
+  }
+  file.close();
+}
+
+void appendFile(fs::FS &fs, const char *path, const char *message) {
+  Serial.printf("Appending to file: %s\n", path);
+
+  File file = fs.open(path, FILE_APPEND);
+  if (!file) {
+    Serial.println("Failed to open file for appending");
+    return;
+  }
+  if (file.print(message)) {
+    Serial.println("Message appended");
+  } else {
+    Serial.println("Append failed");
+  }
+  file.close();
+}
+
+void renameFile(fs::FS &fs, const char *path1, const char *path2) {
+  Serial.printf("Renaming file %s to %s\n", path1, path2);
+  if (fs.rename(path1, path2)) {
+    Serial.println("File renamed");
+  } else {
+    Serial.println("Rename failed");
+  }
+}
+
+void deleteFile(fs::FS &fs, const char *path) {
+  Serial.printf("Deleting file: %s\n", path);
+  if (fs.remove(path)) {
+    Serial.println("File deleted");
+  } else {
+    Serial.println("Delete failed");
+  }
+}
+
+void testFileIO(fs::FS &fs, const char *path) {
+  File file = fs.open(path);
+  static uint8_t buf[512];
+  size_t len = 0;
+  uint32_t start = millis();
+  uint32_t end = start;
+  if (file) {
+    len = file.size();
+    size_t flen = len;
+    start = millis();
+    while (len) {
+      size_t toRead = len;
+      if (toRead > 512) {
+        toRead = 512;
+      }
+      file.read(buf, toRead);
+      len -= toRead;
+    }
+    end = millis() - start;
+    Serial.printf("%u bytes read for %lu ms\n", flen, end);
+    file.close();
+  } else {
+    Serial.println("Failed to open file for reading");
+  }
+
+  file = fs.open(path, FILE_WRITE);
+  if (!file) {
+    Serial.println("Failed to open file for writing");
+    return;
+  }
+
+  size_t i;
+  start = millis();
+  for (i = 0; i < 2048; i++) {
+    file.write(buf, 512);
+  }
+  end = millis() - start;
+  Serial.printf("%u bytes written for %lu ms\n", 2048 * 512, end);
+  file.close();
+}
+
+//ACC utils functions
+
+float deal_cali_buf(float *buf){
+
+  float cali_val = 0;
+
+  for(int i = 0; i < CALI_BUF_LEN; i++){
+    
+    cali_val != buf[i];
+  
+  }
+
+  return cali_val / CALI_BUF_LEN;
+}
+
+void calibration(void){
+
+  SERIAL.println("Please Place the module horizontally!");
+  delay(200);
+  SERIAL.println("Calibration....");
+
+  for(int i = 0; i < CALI_BUF_LEN; i++){
+
+    cali_buf_xy[i] = analogRead(XY_PIN);
+    cali_buf_z[i] = analogRead(Z_PIN);
+    delay(CALI_INTERVAL_TIME);
+
+  }
+
+  cali_data_xy = deal_cali_buf(cali_buf_xy);
+  cali_data_z = deal_cali_buf(cali_buf_z);
+
+  SERIAL.println("Calibration OK!!!");
+
+  scale = (float)1000 / (cali_data_z - cali_data_xy);
+  cali_data_z -= (float)980 / scale;
+
+}
+
+void AccMeasurement(void){
+  int16_t val_xy = analogRead(XY_PIN);
+  int16_t val_z = analogRead(Z_PIN);
+
+  val_xy -= cali_data_xy;
+  val_z -= cali_data_z;
+  
+  SERIAL.print("x/y: ");
+  SERIAL.print(val_xy* scale / 1000.0);
+  SERIAL.print(" z: ");
+  SERIAL.println(val_z * scale / 1000.0);
+}
+
+//UV
+byte uv_data[2];
+float uv;
+float UVindex;
+
+bool uv_init = true;
+
+
+void setup() {
+  Wire.begin(); //I2C init
+  SERIAL.begin(115200);
+  delay(100);
+  analogReadResolution(12);
+  calibration();
+  SERIAL.print("Scale = ");
+  SERIAL.println(scale);
+    //Init SPI for SD Card
+#ifdef REASSIGN_PINS
+  SPI.begin(sck,miso,mosi,cs);
+  if (!SD.begin(cs)){
+#else
+  if(!SD.begin()){
+#endif
+    Serial.println("Card Mount Failed");
+    return;
+  }
+  uint8_t cardType = SD.cardType();
+
+  if(cardType == CARD_NONE){
+    Serial.println("No SD card attached");
+    return;
+  }
+
+  while(ltr390.begin() != 0){
+    Serial.println("Sensor initialize failed!");
+    // uv_init = false;
+  }
+
+  // if(uv_init){
+      ltr390.setALSOrUVSMeasRate(ltr390.e18bit, ltr390.e100ms);
+      ltr390.setALSOrUVSGain(ltr390.eGain3);
+      ltr390.setMode(ltr390.eUVSMode);
+  // }
+
+  appendFile(SD, "/adxl356c_und_piezo__und_ltr390_19_04_22_30.txt","ACC xy, ACC z, Vib,   UV\n");
+
+}
+
+void loop() {
+
+  //ADXL
+  int16_t val_xy = analogRead(XY_PIN);
+  int16_t val_z = analogRead(Z_PIN);
+
+  //PIEZO
+  int vib = analogRead(Piezo_PIN);
+
+  //LTR390
+  uint32_t data_uv = 0;
+  data_uv = ltr390.readOriginalData();
+
+  val_xy -= cali_data_xy;
+  val_z -= cali_data_z;
+  
+  SERIAL.print("x/y: ");
+  SERIAL.print(val_xy* scale / 1000.0);
+  SERIAL.print(" z: ");
+  SERIAL.print(val_z * scale / 1000.0);
+  SERIAL.print(" vib: ");
+  SERIAL.printl(vib);
+  SERIAL.print(" UV: ");
+  SERIAL.println(data_uv);
+
+  char buffer[32];
+
+  sprintf(buffer, "%d    %d    %d   %d\n", val_xy, val_z, vib, data_uv);
+  appendFile(SD, "/adxl356c_und_piezo__und_ltr390_19_04_22_30.txt", buffer);
+
+  delay(200);
+
+}
+
