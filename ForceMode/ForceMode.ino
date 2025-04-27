ab659750840cf636da18bc95189c8b95c181f44d
Mariusz Jacek <marios.yaceg@gmail.com>
2025-04-27 15:51:45 +0200

    ForceMode

--- ForceMode/ForceMode.ino
+++ ForceMode/ForceMode.ino
@@ -6,7 +6,7 @@
  */
 
 #include "Arduino.h"
-#include <ctime>
+// #include <ctime>
 #include <iostream>
 #include <numeric>
 #include "bme68xLibrary.h"
@@ -15,8 +15,8 @@
 #include "SD.h"
 #include "SPI.h"
 
-// #include "DFRobot_LTR390UV.h"
-// DFRobot_LTR390UV ltr390(/*addr = */LTR390UV_DEVICE_ADDR, /*pWire = */&Wire);
+#include "DFRobot_LTR390UV.h"
+DFRobot_LTR390UV ltr390(/*addr = */LTR390UV_DEVICE_ADDR, /*pWire = */&Wire);
 
 #ifndef PIN_CS
 #define PIN_CS 15
@@ -25,6 +25,30 @@
 #define ADD_I2C 0x77
 // #define ADXL 0x53 //accelerometer, 83 in DEC, alt. 0x1C, 0x53
 // #define MCP 0x18 //termometr wewnątrz modułu
+
+//ADXL356C
+#define SERIAL Serial
+#define SYS_VOL 3.3
+
+#define MODULE_RANGE 20
+#define MODULE_VOL 1.8
+
+#define CALI_BUF_LEN        15
+#define CALI_INTERVAL_TIME 100
+
+float cali_buf_xy[CALI_BUF_LEN];
+float cali_buf_z[CALI_BUF_LEN];
+
+float cali_data_xy;
+float cali_data_z;
+int16_t scale;
+
+#define XY_PIN 34
+#define Z_PIN  35
+#define piezoPin 33
+#define LED_PIN 4
+
+// int successfulInits = 0; // Licznik poprawnych inicjalizacji
 
 //function headers 
 template <typename T>
@@ -41,6 +65,90 @@ void appendFile(fs::FS &fs, const char * path, T message){
   file.close();
 }
 
+//Zliczanie plików
+int countFiles(fs::FS &fs, const char *dirname) {
+  File root = fs.open(dirname);
+  if (!root || !root.isDirectory()) {
+    return 0;
+  }
+
+  int fileCount = 0;
+  File file = root.openNextFile();
+  while (file) {
+    if (!file.isDirectory()) {
+      fileCount++;
+    }
+    file = root.openNextFile();
+  }
+  return fileCount;
+}
+
+//UV utils functions
+// Oblicz indeks UV w zakresie 0-11 (przybliżenie)
+int mapUVIndex(uint32_t uv_raw) {
+  // Skalowanie - dostosuj w zależności od typowego zakresu
+  float scaled = (float)uv_raw / 1800.0 * 11.0;
+  if (scaled > 11.0) scaled = 11.0;
+  return (int)(scaled + 0.5);
+}
+
+//ACC utils functions
+
+float deal_cali_buf(float *buf){
+
+  float cali_val = 0;
+
+  for(int i = 0; i < CALI_BUF_LEN; i++){
+    
+    cali_val != buf[i];
+  
+  }
+
+  return cali_val / CALI_BUF_LEN;
+}
+
+void calibration(void){
+
+  SERIAL.println("Please Place the module horizontally!");
+  delay(200);
+  SERIAL.println("Calibration....");
+
+  for(int i = 0; i < CALI_BUF_LEN; i++){
+
+    cali_buf_xy[i] = analogRead(XY_PIN);
+    cali_buf_z[i] = analogRead(Z_PIN);
+    delay(CALI_INTERVAL_TIME);
+
+  }
+
+  cali_data_xy = deal_cali_buf(cali_buf_xy);
+  cali_data_z = deal_cali_buf(cali_buf_z);
+
+  SERIAL.println("Calibration OK!!!");
+
+  digitalWrite(LED_PIN, HIGH);
+  delay(500);
+  digitalWrite(LED_PIN, LOW);
+
+
+  scale = (float)1000 / (cali_data_z - cali_data_xy);
+  cali_data_z -= (float)980 / scale;
+
+}
+
+// void AccMeasurement(void){
+//   int16_t val_xy = analogRead(XY_PIN);
+//   int16_t val_z = analogRead(Z_PIN);
+
+//   val_xy -= cali_data_xy;
+//   val_z -= cali_data_z;
+  
+//   SERIAL.print("x/y: ");
+//   SERIAL.print(val_xy* scale / 1000.0);
+//   SERIAL.print(" z: ");
+//   SERIAL.println(val_z * scale / 1000.0);
+// }
+
 // int16_t * readADXL(int8_t * accelertations_raw);
 void writeFile(fs::FS &fs, const char * path, const char * message);
 void testFileIO(fs::FS &fs, const char * path);
@@ -48,15 +156,15 @@ void createDir(fs::FS &fs, const char * path);
 void listDir(fs::FS &fs, const char * dirname, uint8_t levels);
 
 //global variables
-const char* logfile = "/LOGFILE.txt";
+char logFileName[32];
 float intTemp;
 Bme68x bme;
 
 
-// byte uv_data[2]; //pomocnicza do wczytania danych
-// float uv; //UV z czujnika cyfrowego
-// float analogUv; //UV z czujnika analogowego
-// float UVindex; //przeliczony indeks UV
+byte uv_data[2]; //pomocnicza do wczytania danych
+float uv; //UV z czujnika cyfrowego
+float analogUv; //UV z czujnika analogowego
+float UVindex; //przeliczony indeks UV
 
 
 // int8_t accelerations_raw[6]; //przyspieszenia x, y, z
@@ -66,6 +174,11 @@ Bme68x bme;
  */
 void setup(void)
 {
+  Serial.begin(115200);
+  pinMode(LED_PIN, OUTPUT);
+  digitalWrite(LED_PIN, LOW);
+
+
   Wire.begin();     //I2C mode
 	Serial.begin(115200);
 	
@@ -94,13 +207,43 @@ void setup(void)
 	/* Set the heater configuration to 300 deg C for 100ms for Forced mode */
 	bme.setHeaterProf(300, 100);
 
+  bme.setOpMode(BME68X_FORCED_MODE);
+	delay(500+bme.getMeasDur()/200);
+
+  if(bme.fetchData()){
+    digitalWrite(LED_PIN, HIGH);
+    delay(500);
+    digitalWrite(LED_PIN, LOW);
+    delay(500);
+  }
+
   //SD setup
 	if(!SD.begin(5)){
     Serial.println("Card Mount Failed");
     return;
+  }else{
+    digitalWrite(LED_PIN, HIGH);
+    delay(1000);
+    digitalWrite(LED_PIN, LOW);
+    delay(500);
   }
-  writeFile(SD, logfile, "Start Log");
-  appendFile(SD, logfile, "\n");
+  int fileCount = countFiles(SD, "/");
+
+  // Utwórz nazwę nowego pliku
+  sprintf(logFileName, "/stratos_%d.txt", fileCount);
+  Serial.print("Nowy plik logu: ");
+  Serial.println(logFileName);
+
+
+  writeFile(SD, logFileName, "Start Log");
+  appendFile(SD, logFileName, "\n");
+
+   delay(100);
+  analogReadResolution(12);
+  calibration();
+  delay(1700);
+  SERIAL.print("Scale = ");
+  SERIAL.println(scale);
 
   // //ADXL setup
   // Serial.print("SETUP");
@@ -114,25 +257,25 @@ void setup(void)
   // Wire.write(0x08); //Tryb pomiaru ciągłego
   // Wire.endTransmission();
 
-// //MCP setup
-//   Serial.print("MCP setup");
-//   Wire.beginTransmission(MCP);
-//   Wire.write(0x05); //CONFIG
-//   Wire.write(0x00); //pomiar ciągły
-//   Wire.endTransmission();
 
+  //UV setup
+  if(ltr390.begin() != 0){
+    Serial.println(" Sensor initialize failed!!");
+    delay(100);
+  }else{
+    Serial.println(" Sensor  initialize success!!");
+    ltr390.setALSOrUVSMeasRate(ltr390.e18bit,ltr390.e100ms);//18-bit data, sampling time of 100ms 
+    ltr390.setALSOrUVSGain(ltr390.eGain3);//Gain of 3
+    ltr390.setMode(ltr390.eUVSMode);//Set UV mode 
 
-  // //UV setup
-  // while(ltr390.begin() != 0){
-  //   Serial.println(" Sensor initialize failed!!");
-  //   delay(1000);
-  // }
-  // Serial.println(" Sensor  initialize success!!");
-  // ltr390.setALSOrUVSMeasRate(ltr390.e18bit,ltr390.e100ms);//18-bit data, sampling time of 100ms 
-  // ltr390.setALSOrUVSGain(ltr390.eGain3);//Gain of 3
-  // ltr390.setMode(ltr390.eUVSMode);//Set UV mode 
+    digitalWrite(LED_PIN, HIGH);
+    delay(500);
+    digitalWrite(LED_PIN, LOW);
+    delay(200);
 
-  appendFile(SD, logfile, "accX, accY, accZ, intTemp, extTemp, extHum, extPress, gasRes, extUV\n");
+  }
+
+  appendFile(SD, logFileName, "status ,temp, press, hum, gasRes, ACC xy, ACC z, Vib, UV\n");
 }
 
 void loop(void)
@@ -159,36 +302,6 @@ void loop(void)
   //  Serial.print(fromADXL_ptr[2] * 0.031);
   //  Serial.print("\n");
 
-  // //fetch MCP (internal temperature) data
-  // if(intTemp==-1){
-
-  // Wire.beginTransmission(MCP);
-  // Wire.write(0x05); //CONFIG
-  // Wire.write(0x00); //pomiar ciągły
-  // Wire.endTransmission();
-  // } 
-
-  // Wire.beginTransmission(MCP);
-  // Wire.write(0x05); //rejestr temperatury bezwzględnej
-  // if(!Wire.endTransmission()){
-  //   Wire.requestFrom(MCP, 2);
-
-  //   uint16_t temp = Wire.read() << 8 | Wire.read();
-  //   intTemp = (temp & 0x0FFF) / 16.0;
-  //   if (temp & 0x1000) intTemp -= 256;
-
-  //   Serial.print("Internal temperature: ");
-  //   Serial.print(intTemp);
-  //   Serial.println(" *C");
-  //   appendFile(SD, logfile, intTemp);
-  //   appendFile(SD, logfile, ", ");
-  // } else {
-  //   Serial.println("Internal thermometer disconnected");
-  //   intTemp = -1;
-  //   appendFile(SD, logfile, intTemp);
-  //   appendFile(SD, logfile, ", ");
-  // }
-
   //fetch BME688 data
 	bme68xData data;
 
@@ -198,33 +311,68 @@ void loop(void)
 	if (bme.fetchData())
 	{
 		bme.getData(data);
-		Serial.print(String(millis()) + ", ");
+		// Serial.print(String(millis()) + ", ");
+    Serial.println(data.status, HEX);
 		Serial.print(String(data.temperature) + ", ");
 		Serial.print(String(data.pressure) + ", ");
 		Serial.print(String(data.humidity) + ", ");
 		Serial.print(String(data.gas_resistance) + ", ");
-		Serial.println(data.status, HEX);
 
-    appendFile(SD, logfile, data.temperature);
-    appendFile(SD, logfile, ", ");
-    appendFile(SD, logfile, data.pressure);
-    appendFile(SD, logfile, ", ");
-    appendFile(SD, logfile, data.humidity);
-    appendFile(SD, logfile, ", ");
-    appendFile(SD, logfile, data.gas_resistance);
-    appendFile(SD, logfile, ", ");
-    appendFile(SD, logfile, data.status);
+    appendFile(SD, logFileName, data.status);
+    appendFile(SD, logFileName, ", ");
+    appendFile(SD, logFileName, data.temperature);
+    appendFile(SD, logFileName, ", ");
+    appendFile(SD, logFileName, data.pressure);
+    appendFile(SD, logFileName, ", ");
+    appendFile(SD, logFileName, data.humidity);
+    appendFile(SD, logFileName, ", ");
+    appendFile(SD, logFileName, data.gas_resistance);
 	}
 
-  // uint32_t data_uv = 0;
-  // data_uv= ltr390.readOriginalData();//Get UV raw data
-  // Serial.print("data:");
-  // Serial.println(data_uv);
-  // delay(1000);
-  // appendFile(SD, logfile, ", ");
-  // appendFile(SD, logfile, data_uv);
+    //ADXL356C
+  // int16_t val_xy = analogRead(XY_PIN);
+  // int16_t val_z = analogRead(Z_PIN);
 
-  appendFile(SD, logfile, "\n");
+  // val_xy -= cali_data_xy;
+  // val_z -= cali_data_z;
+  int16_t val_xy = analogRead(XY_PIN);
+  int16_t val_z = analogRead(Z_PIN);
+
+  val_xy -= cali_data_xy;
+  val_z -= cali_data_z;
+  
+  SERIAL.print(",x/y: ");
+  SERIAL.print(val_xy* scale / 1000.0);
+  SERIAL.print(", z: ");
+  SERIAL.print(val_z * scale / 1000.0);
+
+  char acc_buffer[32];
+
+  sprintf(acc_buffer, "   %f, %f", val_xy* scale / 1000.0, val_z * scale / 1000.0);
+  appendFile(SD, logFileName, acc_buffer);
+
+
+
+  //Piezo
+  int vib = analogRead(piezoPin);
+  SERIAL.println(", ");
+  SERIAL.println(vib);
+  char vib_buffer[32];
+  sprintf(vib_buffer, ",%d",vib);
+  appendFile(SD, logFileName, vib_buffer);
+
+
+  //UV
+
+  uint32_t data_uv = 0;
+  data_uv= ltr390.readOriginalData();//Get UV raw data
+  int uv_index = mapUVIndex(data_uv);
+  SERIAL.println(", ");
+  Serial.println(data_uv);
+  char uv_buffer[32];
+  sprintf(uv_buffer, ", %d", uv_index);
+  appendFile(SD, logFileName, uv_buffer);
+  appendFile(SD, logFileName, "\n");
 
   
 }
